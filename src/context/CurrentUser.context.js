9d187efad1e4e5ecfee22ac5a81664c54920d25e
Czuczen <czuczengames@gmail.com>
2020-05-31 20:10:39 +0200

    first commit

---
+++ src/context/CurrentUser.context.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+const CurrentUserContext = React.createContext();
+
+export class CurrentUserProvider extends Component {
+    state = {
+        user: null
+    }
+
+    login = () =>
+    {
+        this.setState({
+            user: {
+                name: ""
+            }
+        })
+    }
+
+    logout = () =>
+    {
+        this.setState({user: null})
+    }
+
+    render(){
+        const { children } = this.props;
+        return(
+            <CurrentUserContext.Provider
+            value={{
+                login: this.login,
+                logout: this.logout,
+                user: this.state.user
+            }}
+            >
+                { children }
+            </CurrentUserContext.Provider>
+        )
+    }
+}
+
+export const CurrentUserConsumer = CurrentUserContext.Consumer;
